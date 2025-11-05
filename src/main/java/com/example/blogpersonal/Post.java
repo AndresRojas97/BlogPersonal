dfef07017d124ccadcadc22df6551988bbfe10bf
AndresRojas97 <andres.rojascontacto@gmail.com>
2025-11-05 17:12:43 -0300

    agregando comentarios

--- src/main/java/com/example/blogpersonal/Post.java
+++ src/main/java/com/example/blogpersonal/Post.java
@@ -17,9 +17,18 @@ public class Post {
     private String title;
     @Lob
     private String content;
+
+    //@JoinColumn(name = "user_id"): Esto quiere decir que en la tabla post habra una columna llamada user_id que es la foreign key
+    //@ManyToOne: "Muchos Post (esta clase) pertenecen a Un User (la variable user)".
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
+
+    //En mappedBy le estamos diciendo a JPA que vaya a la clase Comment busque la variable post para buscar los comentarios almacenados en esta tabla.
+    //En cascade = CascadeType ALL cuando guardas un post tambien guardara los comentarios de dicho post o como tambien si eliminas el post tambien eliminara los comentarios de ese post
+    //orphanRemoval = true, al eliminar un comentario lo eliminas de la lista Comment, pero no de la base de datos, por lo tanto, esto asegura que tambien sera eliminado de la base de datos.
+
+
     @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Comment> comments;
     @CreationTimestamp
